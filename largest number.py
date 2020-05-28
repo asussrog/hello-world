0089f8a2b79b73fa8387dbb2528e2299d76de90d
asussrog <fragmnn@gmail.com>
2020-05-28 12:33:00 -0400

    program to find max n min number

---
+++ largest number.py
@@ -0,0 +1,8 @@
+list = []
+num = int(input("How many numbers:"))
+
+for n in range(num):
+    numbers = int(input("Enter number: \b"))
+    list.append(numbers)
+print("Max element is:", max(list), "\n Min element is:",min(list))
+# ctrl + / tuhain muriig comment bolgoh
